c65b3bec5b57a98de4813e9fdcd8337b35c851cf
Wolfgang Richter <wolf@cs.cmu.edu>
2014-08-25 15:45:48 -0400

    introspection-test.py: live test code

--- introspection-test.py
+++ introspection-test.py
@@ -12,14 +12,6 @@ basicConfig()
 LOG = getLogger('introspection-test')
 LOG.setLevel(DEBUG)
 
-def try_call(fn, *args, **kwargs):
-    try:
-        print fn(*args, **kwargs)
-    except HTTPNotImplemented:
-        LOG.error('<%s> HTTPNotImplemented returned by server!' % fn.__name__)
-    except NotFound:
-        LOG.error('<%s> NotFound returned by server!' % fn.__name__)
-
 if __name__ == '__main__':
     user = raw_input('Username: ')
     tenant = raw_input('Tenant: ')
@@ -39,10 +31,16 @@ if __name__ == '__main__':
               'URL "%s"', user, tenant, url)
     c = client.Client(user, pw, tenant, url, service_type='compute')
 
-    print c.servers.list()[0].to_dict()
+    print '> Testing introspection.create'
+    entity = c.introspection.create(instance, drive_id='virtio0',
+                        introspection_target='gammaray').introspected_entity_id
+    print '\tCreated introspection instance <%s>' % entity
 
-    try_call(c.introspection.list, instance)
-    try_call(c.introspection.get, instance, entity)
-    try_call(c.introspection.create, instance, drive_id='virtio0',
-                                               introspection_target='gammaray')
-    try_call(c.introspection.delete, instance, entity)
+    print '> Testing introspection.list'
+    print '\t' + str(c.introspection.list(instance))
+
+    print '> Testing introspection.get'
+    print '\t' + str(c.introspection.get(instance, entity))
+
+    print '> Testing introspection.delete'
+    print '\t' + str(c.introspection.delete(instance, entity))
